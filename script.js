1342b00acffb3403c1219bbfd9c70e7b9a95b4ad
Drew Anello <drew.anello@gmail.com>
2023-05-05 13:19:37 -0400

    updates EventListener"

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-// Get the navbar element
-const navbar = document.querySelector('.nav-wrap');
-
-// Add a scroll event listener to the window
-window.addEventListener('scroll', () => {
-  // Check if the user has scrolled
-  if (window.pageYOffset > 0) {
-    // Add a class to the navbar to hide it
-    navbar.classList.add('hide');
-  } else {
-    // Remove the class from the navbar to show it
-    navbar.classList.remove('hide');
-  }
-});
-
 const newTab = document.getElementById('newTab')
+
 const toolTip = document.getElementById("toolTip");
 
 
@@ -31,3 +17,9 @@ newTab.addEventListener("mouseleave", function(e) {
   }
 })
 
+newTab.addEventListener("click", function(e) {
+  const message = document.querySelector(".message")
+  if (message) {
+    message.remove()
+  }
+})
