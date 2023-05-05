0c23ed62635e6f1c70331a180f30be1f77642c47
Drew Anello <drew.anello@gmail.com>
2023-05-05 13:01:45 -0400

    adds hover button message and styles

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ window.addEventListener('scroll', () => {
     navbar.classList.remove('hide');
   }
 });
+
+const newTab = document.getElementById('newTab')
+const toolTip = document.getElementById("toolTip");
+
+
+newTab.addEventListener('mouseenter', function(e) {
+  const message = document.createElement('div')
+    message.textContent = "This will open in a new tab with my resume 😎"
+    message.classList.add('message')
+    newTab.appendChild(message)
+});
+
+newTab.addEventListener("mouseleave", function(e) {
+  const message = document.querySelector(".message")
+  if (message) {
+    message.remove()
+  }
+})
+
